74756f5e61c78483ea4d9dc39716cf6b7089ea2f
Gregório Melo <gregorio@gregoriomelo.com>
2014-03-31 14:08:32 +0800

    Adding some tests

---
+++ tests/unit/models/book-test.js
@@ -0,0 +1,12 @@
+import { test, moduleFor, moduleForModel } from 'ember-qunit';
+import Book from 'appkit/models/book';
+
+moduleForModel('book', 'Book model');
+
+test("Book is a valid ember-data model", function() {
+  var book = this.subject({title: "TDD by Example", isRead: false});
+  ok(book);
+  ok(false);
+  ok(book instanceof DS.Model);
+  ok(book instanceof Book);
+});
---
+++ tests/unit/routes/books-test.js
@@ -0,0 +1,14 @@
+import { test, moduleFor } from 'ember-qunit';
+
+
+import BooksRoute from 'appkit/routes/books';
+
+moduleFor('route:books', "Unit - BooksRoute");
+
+test("it exists", function(){
+  ok(this.subject() instanceof BooksRoute);
+});
+
+test("#model", function(){
+  equal(this.subject().model().FIXTURES.length, 3);
+});
